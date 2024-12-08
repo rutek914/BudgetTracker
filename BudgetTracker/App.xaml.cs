200169023c0930e555ecc36a58e5444b9dc29033
rucinski_p <rutek914@hotmail.com>
2024-12-08 23:18:43 +0100

    added DI

--- BudgetTracker/App.xaml.cs
+++ BudgetTracker/App.xaml.cs
@@ -1,14 +1,44 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace BudgetTracker
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        // obiekt ten odpowiada za zarządzanie cyklem życia aplikacji i usług (DI container + extras)
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                 .ConfigureServices((hostContext, services) =>
+                 {
+                     services.AddSingleton<MainWindow>();
+                 })
+                 .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            // rozpoczyna działanie hosta, uruchomienie  usług skonfigurowanych w host
+            await AppHost!.StartAsync();
+
+            // apphost.services to kontener DI który zawiera wszystkie zarejestrowane usługi i obiekty
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startupForm.Show();
+            // wywołuje wersje metody onstartup z klasy bazowej Application
+            base.OnStartup(e);
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
+
     }
 
 }
